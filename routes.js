70fc559eb7ac5040653f3a2db60dbef71e22d286
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-17 08:52:44 -0700

    Added links handlers on the server.

--- routes.js
+++ routes.js
@@ -1,3 +1,13 @@
 module.exports = function (app) {
+  var links = [];
 
+  app.post('/links', function (req, res) {
+    console.log("Received:", req.body);
+    links.unshift(req.body);
+    res.send(200, req.body);
+  });
+
+  app.get('/links', function (req, res) {
+    res.json(JSON.stringify(links));
+  });
 };
